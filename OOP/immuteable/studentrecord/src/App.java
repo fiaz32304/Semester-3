4df430f294a1fd02a2a6e48deae09e48138919ba
Fiaz ahmad <fiazahmadf1983@gmail.com>
2025-10-19 21:35:53 +0500

    update

---
+++ OOP/immuteable/studentrecord/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        studentrecord s1=new studentrecord("Fiaz Ahmad",34);
+        System.out.println("the name is"+s1.getname());
+        System.out.println("roll no is"+s1.getrollno());
+        
+    }
+}
---
+++ OOP/immuteable/studentrecord/src/studentrecord.java
@@ -0,0 +1,18 @@
+public class studentrecord {
+    private final String name;
+    private final int rollno;
+
+    studentrecord(String name, int rollno){
+        this.name=name;
+        this.rollno=rollno;
+
+    }
+    public String getname(){
+        return name;
+    }
+    public int getrollno(){
+        return rollno;
+        
+    }
+    
+}
